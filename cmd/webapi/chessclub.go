14ddb5041c9d593d333cb6ffc15e36aae217cf09
Tacio Costa <taciomcosta@gmail.com>
2020-06-29 17:33:53 -0300

    refactor: extract request/response from webapi/chessclub

--- cmd/webapi/chessclub.go
+++ cmd/webapi/chessclub.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
-	"github.com/gorilla/schema"
 	"github.com/taciomcosta/chesstournament/internal/chessclub"
 	"github.com/taciomcosta/chesstournament/internal/model"
 	"github.com/taciomcosta/chesstournament/internal/repository"
@@ -21,41 +17,11 @@ func init() {
 }
 
 func GetChessclubDetailsHandler(w http.ResponseWriter, r *http.Request) {
-	id := getId(r)
-
-	club, err := s.GetClubById(id)
-
+	c, err := s.GetClubById(id(r))
 	if ok := tryRespondWithError(w, http.StatusNotFound, err); ok {
 		return
 	}
-
-	json := mustJSON(club)
-	w.Write(json)
-}
-
-func getId(r *http.Request) int {
-	vars := mux.Vars(r)
-	id, _ := strconv.Atoi(vars["id"])
-	return id
-}
-
-func tryRespondWithError(w http.ResponseWriter, httpStatus int, err error) bool {
-	if err == nil {
-		return false
-	}
-	w.WriteHeader(httpStatus)
-	w.Write(errorResponse(err))
-	return true
-}
-
-func errorResponse(err error) []byte {
-	errString := fmt.Sprintf(`{"msg": "%s"}`, err)
-	return []byte(errString)
-}
-
-func mustJSON(v interface{}) []byte {
-	json, _ := json.Marshal(v)
-	return json
+	respond(w, c)
 }
 
 func CreateChessclubHandler(w http.ResponseWriter, r *http.Request) {
@@ -70,16 +36,14 @@ func CreateChessclubHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	json := mustJSON(c)
 	w.WriteHeader(http.StatusCreated)
-	w.Write(json)
+	respond(w, c)
 }
 
 func readChessclubFromBody(r *http.Request) (*model.ChessClub, error) {
 	c := new(model.ChessClub)
 	b, _ := ioutil.ReadAll(r.Body)
 	r.Body.Close()
-
 	err := json.Unmarshal(b, c)
 	return c, err
 }
@@ -90,46 +54,29 @@ func EditChessclubHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = s.EditChessclub(getId(r), c)
+	err = s.EditChessclub(id(r), c)
 
 	if ok := tryRespondWithError(w, http.StatusBadRequest, err); ok {
 		return
 	}
 
-	json := mustJSON(c)
 	w.WriteHeader(http.StatusOK)
-	w.Write(json)
+	respond(w, c)
 }
 
 func ListChessclubsHandler(w http.ResponseWriter, r *http.Request) {
-	f := getFilter(r)
-
+	f := newFilter(r)
 	cs, err := s.ListClubs(f)
-
 	if ok := tryRespondWithError(w, http.StatusBadRequest, err); ok {
 		return
 	}
-
-	json := mustJSON(cs)
-	w.Write(json)
-}
-
-func getFilter(r *http.Request) repository.Filter {
-	var f repository.Filter
-	r.ParseForm()
-	schema.NewDecoder().Decode(&f, r.Form)
-	return f
+	respond(w, cs)
 }
 
 func DeleteChessclubHandler(w http.ResponseWriter, r *http.Request) {
-	id := getId(r)
-
-	c, err := s.DeleteClub(id)
-
+	c, err := s.DeleteClub(id(r))
 	if ok := tryRespondWithError(w, http.StatusNotFound, err); ok {
 		return
 	}
-
-	json := mustJSON(*c)
-	w.Write(json)
+	respond(w, c)
 }
