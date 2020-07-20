20e600953569f415865abfc22d7b5fe008d3afe0
Tacio Costa <taciomcosta@gmail.com>
2020-07-20 18:11:55 -0300

    refactor: join handler files

--- cmd/webapi/handlers.go
+++ cmd/webapi/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/taciomcosta/chesstournament/internal/model"
@@ -55,4 +57,41 @@ func DeleteChessclubHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	respond(w, c)
+}
+
+func CreatePlayerHandler(w http.ResponseWriter, r *http.Request) {
+	p := readPlayerFromBody(r)
+	_, err := s.CreatePlayer(p)
+	if ok := tryRespondWithError(w, http.StatusBadRequest, err); ok {
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	respond(w, p)
+}
+
+func readPlayerFromBody(r *http.Request) *model.Player {
+	p := new(model.Player)
+	unmarshalJsonBody(r, p)
+	return p
+}
+
+func unmarshalJsonBody(r *http.Request, v interface{}) {
+	b, _ := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	json.Unmarshal(b, v)
+}
+
+func GetPlayerDetailsHandler(w http.ResponseWriter, r *http.Request) {
+	f := func(id int) (interface{}, error) { return s.GetPlayerById(id) }
+	getDetails(w, r, f)
+}
+
+type getDetailsFunc func(id int) (interface{}, error)
+
+func getDetails(w http.ResponseWriter, r *http.Request, get getDetailsFunc) {
+	v, err := get(getIdFromRequest(r))
+	if ok := tryRespondWithError(w, http.StatusNotFound, err); ok {
+		return
+	}
+	respond(w, v)
 }
---
+++ cmd/webapi/handlers_test.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/taciomcosta/chesstournament/internal/data"
+	"github.com/taciomcosta/chesstournament/internal/shared"
+)
+
+func TestMain(m *testing.M) {
+	s = shared.NewService(&data.MockRepository{}, &data.MockChessClubRepository{}, data.MockPlayerRepository{})
+	os.Exit(m.Run())
+}
+
+type handlerTest struct {
+	description    string
+	request        *http.Request
+	handle         http.HandlerFunc
+	expectedStatus int
+}
+
+var handlerTests []handlerTest = []handlerTest{
+	{
+		description:    "should handle details of existing Chessclub",
+		request:        newRequestBuilder().withPathVar("id", "1").build(),
+		handle:         GetChessclubDetailsHandler,
+		expectedStatus: http.StatusOK,
+	},
+	{
+		description:    "should handle details of unexistent Chessclub",
+		request:        newRequestBuilder().withPathVar("id", "unexistent").build(),
+		handle:         GetChessclubDetailsHandler,
+		expectedStatus: http.StatusNotFound,
+	},
+	{
+		description:    "should handle creation of Chessclub",
+		request:        newRequestBuilder().withBody(toJSONString(data.MockValidChessClub)).build(),
+		handle:         CreateChessclubHandler,
+		expectedStatus: http.StatusCreated,
+	},
+	{
+		description:    "should handle invalid input on Chessclub creation",
+		request:        newRequestBuilder().withBody(`{"invalid": "body"}`).build(),
+		handle:         CreateChessclubHandler,
+		expectedStatus: http.StatusBadRequest,
+	},
+	{
+		description:    "should handle Chessclubt editing",
+		request:        newRequestBuilder().withBody(toJSONString(data.MockValidChessClub)).withPathVar("id", "1").build(),
+		handle:         EditChessclubHandler,
+		expectedStatus: http.StatusOK,
+	},
+	{
+		description:    "should handle invalid input on Chessclub editing",
+		request:        newRequestBuilder().withBody(toJSONString(`{"id":"1"}`)).withPathVar("id", "1").build(),
+		handle:         EditChessclubHandler,
+		expectedStatus: http.StatusBadRequest,
+	},
+	{
+		description:    "should list Chessclubs",
+		request:        newRequestBuilder().build(),
+		handle:         ListChessclubsHandler,
+		expectedStatus: http.StatusOK,
+	},
+	{
+		description:    "should handle invalid filter options on Chessclub listing",
+		request:        newRequestBuilder().withQueryParam("$orderBy", "invalid").build(),
+		handle:         ListChessclubsHandler,
+		expectedStatus: http.StatusBadRequest,
+	},
+	{
+		description:    "should handle Chessclub deletion",
+		request:        newRequestBuilder().withPathVar("id", "1").build(),
+		handle:         DeleteChessclubHandler,
+		expectedStatus: http.StatusOK,
+	},
+	{
+		description:    "should handle deletion of unexistent Chessclub",
+		request:        newRequestBuilder().withPathVar("id", "-1").build(),
+		handle:         DeleteChessclubHandler,
+		expectedStatus: http.StatusNotFound,
+	},
+	{
+		description:    "should handle details of existing Player",
+		request:        newRequestBuilder().withPathVar("id", "1").build(),
+		handle:         GetPlayerDetailsHandler,
+		expectedStatus: http.StatusOK,
+	},
+	{
+		description:    "should handle details of unexistent Player",
+		request:        newRequestBuilder().withPathVar("id", "unexistent").build(),
+		handle:         GetPlayerDetailsHandler,
+		expectedStatus: http.StatusNotFound,
+	},
+	{
+		description:    "should handle creation of Player",
+		request:        newRequestBuilder().withBody(toJSONString(data.MockValidPlayer)).build(),
+		handle:         CreatePlayerHandler,
+		expectedStatus: http.StatusCreated,
+	},
+	{
+		description:    "should handle invalid input on Player creation",
+		request:        newRequestBuilder().withBody(`{"invalid": "body"}`).build(),
+		handle:         CreatePlayerHandler,
+		expectedStatus: http.StatusBadRequest,
+	},
+}
+
+func TestHandlers(t *testing.T) {
+	for _, test := range handlerTests {
+		testHandler(t, test)
+	}
+}
+
+func testHandler(t *testing.T, test handlerTest) {
+	recorder := httptest.NewRecorder()
+	test.handle(recorder, test.request)
+	if recorder.Code != test.expectedStatus {
+		t.Error(test.description)
+	}
+}
